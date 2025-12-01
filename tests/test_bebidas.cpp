2ba6ee379c8a57046e438d4f100258ae59f48b77
Wendy <a01426507@tec.mx>
2025-12-01 10:36:54 -0600

    test:test_bebidas.cpp

---
+++ tests/test_bebidas.cpp
@@ -0,0 +1,10 @@
+#include "Bebida.h"
+#include <cassert>
+#include <string>
+using namespace std;
+
+int main(){
+    Bebida bebida1("boing mango", 15.0, "chico");
+    bebida1.servir();
+    assert(bebida1.getEstado() == "sevida");
+}
