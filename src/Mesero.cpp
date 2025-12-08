f415281fe40989d9fe2f77f5238787222baa26fc
Wendy <a01426507@tec.mx>
2025-12-08 10:49:32 -0600

    fix:agregar implementacion

--- src/Mesero.cpp
+++ src/Mesero.cpp
@@ -5,10 +5,19 @@ using namespace std;
 Mesero::Mesero() : Empleado() {
     bebidasPreparadas = 0;
 }
+
 int Mesero::getBebidasPreparadas() {
     return bebidasPreparadas;
 }
 void Mesero::PrepararBebida(Bebida:bebida) {
     bebida.servir();
     
+}
+Mesero::Mesero(string nombre, int edad, string telefono, int id)
+{
+    nombre = nombre;
+    edad = edad;
+    telefono = telefono;
+    id = id;
+    bebidasPreparadas = 0;
 }
