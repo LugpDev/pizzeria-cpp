8707bdcc776c4c5edca0759ad2d3c78111dea808
LugpDev2022 <LugpDev2022@gmail.com>
2025-12-07 19:55:49 -0600

    feat(Bebida): añadir más tests

--- tests/test_bebidas.cpp
+++ tests/test_bebidas.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main()
 {
     Bebida bebida1("boing mango", 15.0, "chico");
+    assert(bebida1.getEstado() == "pedido");
     bebida1.servir();
-    assert(bebida1.getEstado() == "Servida");
+    assert(bebida1.getEstado() == "servido");
+    assert(bebida1.getNombre() == "boing mango");
+    assert(bebida1.getPrecio() == 15.0);
+    assert(bebida1.getTamanioVaso() == "chico");
 }
