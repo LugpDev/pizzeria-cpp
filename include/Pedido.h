1a9aa3ab98506abb369d09c82cb5c65172ace549
Carlos <a01425194@tec.mx>
2025-12-08 08:34:43 -0600

    feat(Pedido): implementar clase y metodos para gestionar ordenes.

---
+++ include/Pedido.h
@@ -0,0 +1,38 @@
+#ifndef __PEDIDO_H__
+#define __PEDIDO_H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "Producto.h"
+#include "Cliente.h"
+
+class Pedido
+{
+private:
+    int id;
+    string estado;
+    vector<Producto> productos;
+    Cliente cliente;
+    bool pagado;
+
+public:
+    Pedido();
+    Pedido(int _id, string _estado, vector<Producto> _productos, Cliente _cliente, bool _pagado);
+    void añadirProducto(Producto producto);
+    void calcularTotal();
+    void pagar(float pago);
+
+    string getEstado();
+    bool getPagado();
+    int getId();
+    vector<Producto> getProductos();
+
+    void setEstado(string _estado);
+    void setPagado(bool _pagado);
+    void setId(int _id);
+    void setProductos(vector<Producto> _productos);
+
+#endif // __PEDIDO_H__
---
+++ src/Pedido.cpp
@@ -0,0 +1,79 @@
+#include "Pedido.h"
+
+Pedido::Pedido() {}
+Pedido::Pedido(int _id, string _estado, vector<Producto> _productos, Cliente _cliente, bool _pagado)
+{
+    id = _id;
+    estado = _estado;
+    productos = _productos;
+    cliente = _cliente;
+    pagado = _pagado;
+}
+
+void Pedido::añadirProducto(Producto producto)
+{
+    productos.push_back(producto);
+}
+
+void Pedido::calcularTotal()
+{
+    float total = 0.0;
+    for (Producto producto : productos)
+    {
+        total += producto.getPrecio();
+    }
+    cout << "Total del pedido: " << total << endl;
+}
+
+void Pedido::pagar(float pago)
+{
+    float total = 0.0;
+    for (Producto producto : productos)
+    {
+        total += producto.getPrecio();
+    }
+
+    if (pago >= total)
+    {
+        pagado = true;
+        cout << "Pago realizado con éxito." << endl;
+    }
+    else
+    {
+        cout << "Pago insuficiente." << endl;
+    }
+}
+
+string Pedido::getEstado()
+{
+    return estado;
+}
+bool Pedido::getPagado()
+{
+    return pagado;
+}
+int Pedido::getId()
+{
+    return id;
+}
+
+vector<Producto> Pedido::getProductos()
+{
+    return productos;
+}
+void Pedido::setEstado(string _estado)
+{
+    estado = _estado;
+}
+void Pedido::setPagado(bool _pagado)
+{
+    pagado = _pagado;
+}
+void Pedido::setId(int _id)
+{
+    id = _id;
+}
+void Pedido::setProductos(vector<Producto> _productos)
+{
+    productos = _productos;
+}
