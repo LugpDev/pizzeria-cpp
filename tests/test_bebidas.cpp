8d7e405e4d9c74b9991651bbd8cf91e6f7dbf289
LugpDev2022 <LugpDev2022@gmail.com>
2025-12-07 19:58:57 -0600

    feat(Bebida): agregar mensaje de éxito al finalizar pruebas

--- tests/test_bebidas.cpp
+++ tests/test_bebidas.cpp
@@ -1,6 +1,9 @@
 #include "Bebida.h"
+
 #include <cassert>
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 int main()
@@ -12,4 +15,6 @@ int main()
     assert(bebida1.getNombre() == "boing mango");
     assert(bebida1.getPrecio() == 15.0);
     assert(bebida1.getTamanioVaso() == "chico");
+
+    cout << "Pruebas de Bebida pasadas exitosamente." << endl;
 }
