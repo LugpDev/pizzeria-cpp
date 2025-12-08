b86c34daf22105752999007e6d664a67ca9034b3
Diana <a01425668@tec.mx>
2025-12-08 08:30:51 -0600

    feat(Persona): añadir clase Persona

---
+++ Persona.cpp
@@ -0,0 +1,17 @@
+#include "Persona.h"
+
+// Persona Constructor
+Persona::Persona() {};
+Persona::Persona(string nombre, int edad, string telefono)
+    : nombre(nombre), edad(edad), telefono(telefono) {}
+
+// GETTERS
+string Persona::getNombre() {
+    return nombre;
+}
+int Persona::getEdad() {
+    return edad;
+}
+string Persona::getTelefono() {
+    return telefono;
+}
---
+++ Persona.h
@@ -0,0 +1,24 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+using namespace std;
+
+class Persona {
+private:
+    string nombre;
+    int edad;  
+    string telefono;
+
+public:
+
+    // Constructor
+    Persona();
+    Persona(string nombre, int edad, string telefono);
+
+    //Getters
+    string getNombre();
+    int getEdad();
+    string getTelefono();
+};
+#endif
