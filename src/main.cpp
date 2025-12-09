74bc1e007fa7899288c90ef521dbbff961d11ac9
Carlos <a01425194@tec.mx>
2025-12-08 22:46:19 -0600

    feat(main): implement main function to handle user options in Pizzeria

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include "Pizzeria.h"
+
 int main()
 {
+  Pizzeria pizzeria;
+  pizzeria.mostrarOpciones();
+  int opcion;
+
+  std::cout << "Ingrese una opcion: ";
+  std::cin >> opcion;
+  do
+  {
+    pizzeria.ejecutarOpcion(opcion);
+    pizzeria.mostrarOpciones();
+    std::cout << "Ingrese una opcion: ";
+    std::cin >> opcion;
+  } while (opcion != 0);
+
   return 0;
 }
