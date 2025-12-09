61bd6324836dc0be93e64017da187aee37471c44
LugpDev2022 <LugpDev2022@gmail.com>
2025-12-08 20:58:02 -0600

    refactor(mesero): renombrar tests

---
+++ tests/test_mesero.cpp
@@ -0,0 +1,19 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include "Mesero.h"
+using namespace std;
+
+int main()
+{
+    Mesero mesero1;
+    assert(mesero1.getBebidasPreparadas() == 0);
+
+    Bebida bebida1("Coca Cola", 20.0, "grande");
+    assert(bebida1.getEstado() == "pedido");
+
+    mesero1.PrepararBebida(bebida1);
+    assert(bebida1.getEstado() == "servido");
+
+    cout << "Pruebas de Mesero pasadas exitosamente." << endl;
+} 
